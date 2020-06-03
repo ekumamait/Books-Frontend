f8bdcc852f4e7f61836cf8c5697a04d9c91a147b
ekumamait <ericebuluochol@gmail.com>
2020-06-03 17:44:10 +0300

    ch(sass): setup sass processing 	- install node-sass for processing  	- setup file structure for auth
    [Finishes #173106287]

---
+++ books-app/src/actions/AuthActions.js
@@ -0,0 +1,53 @@
+import { GET_ERRORS } from '../constants/ActionTypes';
+
+const baseUrl = process.env.baseUrl;
+const localStorage = window.localStorage;
+
+export const loginUser = (userData, history) => dispatch => {
+  return fetch(`${baseUrl}/login`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    CORS: 'no-cors',
+    body: JSON.stringify(userData)
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if (data.error) {
+        return dispatch({
+          type: GET_ERRORS,
+          payload: data.error
+        });
+      }
+      if (data.access_token){
+        const token = data.access_token;
+        localStorage.setItem('token', token);
+        history.push('/books');
+      }
+    });
+};
+
+export const registerUser = (userData, history) => dispatch => {
+  return fetch(`${baseUrl}/signup`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    CORS: 'no-cors',
+    body: JSON.stringify(userData)
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        return dispatch({
+          type: GET_ERRORS,
+          payload: data.error
+        });
+      }
+      const token = data.access_token;
+      localStorage.setItem('token', token);
+      history.push('/');
+    });
+};
--- books-app/src/reducers/index.js
+++ books-app/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import errorReducer from './ErrorReducer';
+import authReducer from './AuthReducer';
 
 const rootReducer = combineReducers({
+    error: errorReducer,
+    auth: authReducer
 });
 
 export default rootReducer;
